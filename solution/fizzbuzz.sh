6c2cb1a042f77467dc6d504051101aa81e67c9dd
Irina Mamzurina <mamzurina.ira@yandex.ru>
2025-09-30 18:10:00 +0700

    final solution

--- solution/fizzbuzz.sh
+++ solution/fizzbuzz.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 for ((i = 1; i <= $1; i++)); do
-  if !((i % 3)); then
-    if !((i % 5)); then
+  if ((i % 3 == 0)); then
+    if ((i % 5 == 0)); then
       echo "Fizz Buzz"
     else 
       echo "Fizz"
     fi
   else
-    if !((i % 5)); then
+    if ((i % 5 == 0)); then
       echo "Buzz"
     else
       echo "$i"
